e582b2e8e68246e3684f0b1d21f9e22fba5224b3
Enes Egemen Aktı <enesegemenakti@ogr.iu.edu.tr>
2026-02-08 14:11:51 +0300

    Add files via upload

---
+++ exercise_2.2.py
@@ -0,0 +1,55 @@
+
+# solution of a and b
+T = float(input("Enter the time:"))
+G = 6.67e-11
+M = 5.97e24
+R = 6371
+π = 3.14
+h = (G*M*T**2/4*π**2)**1/3 - R
+print("The altitude of the satellite is",h,"meters")
+
+
+# solution of c
+T = 24*60*60
+G = 6.67e-11
+M = 5.97e24
+R = 6371
+π = 3.14
+h = (G*M*T**2/4*π**2)**1/3 - R
+print("The altitude of the satellite is",h,"meters")
+
+T = 90*60
+G = 6.67e-11
+M = 5.97e24
+R = 6371
+π = 3.14
+h = (G*M*T**2/4*π**2)**1/3 - R
+print("The altitude of the satellite is",h,"meters")
+
+T = 45*60
+G = 6.67e-11
+M = 5.97e24
+R = 6371
+π = 3.14
+h = (G*M*T**2/4*π**2)**1/3 - R
+print("The altitude of the satellite is",h,"meters")
+
+# solution of d
+T = 23.93 * 60*60
+G = 6.67e-11
+M = 5.97e24
+R = 6371
+π = 3.14
+h = (G*M*T**2/4*π**2)**1/3 - R
+print("The altitude of the satellite is",h,"meters")
+
+
+
+
+
+
+
+
+
+
+
